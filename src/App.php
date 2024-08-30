3e38e15e9cf8ca3225597a342232cfefa0bdf545
Md Sharif Miah <59281214+thesharifdev@users.noreply.github.com>
2024-08-30 07:39:00 +0600

    implemented: CURD

--- src/App.php
+++ src/App.php
@@ -5,6 +5,7 @@ namespace Sharif\PhpCrudStarterKit;
 class App
 {
     public static $instance;
+    private $jsonFile = "json_files/data.json"; 
 
     /**
      * Singleton instance
@@ -25,7 +26,25 @@ class App
      * @return string
      */
     public function create($data){
-        return $data.' created successfully!';
+        if(!empty($newData)){ 
+            $id = time(); 
+            $newData['id'] = $id; 
+             
+            $jsonData = file_get_contents($this->jsonFile); 
+            $data = json_decode($jsonData, true); 
+             
+            $data = !empty($data)?array_filter($data):$data; 
+            if(!empty($data)){ 
+                array_push($data, $newData); 
+            }else{ 
+                $data[] = $newData; 
+            } 
+            $insert = file_put_contents($this->jsonFile, json_encode($data)); 
+             
+            return $insert?$id:false; 
+        }else{ 
+            return false; 
+        } 
     }
 
     /**
@@ -35,7 +54,32 @@ class App
      * @return string
      */
     public function update($data){
-        return $data.' updated successfully!';
+        if(!empty($upData) && is_array($upData) && !empty($id)){ 
+            $jsonData = file_get_contents($this->jsonFile); 
+            $data = json_decode($jsonData, true); 
+             
+            foreach ($data as $key => $value) { 
+                if ($value['id'] == $id) { 
+                    if(isset($upData['name'])){ 
+                        $data[$key]['name'] = $upData['name']; 
+                    } 
+                    if(isset($upData['email'])){ 
+                        $data[$key]['email'] = $upData['email']; 
+                    } 
+                    if(isset($upData['phone'])){ 
+                        $data[$key]['phone'] = $upData['phone']; 
+                    } 
+                    if(isset($upData['country'])){ 
+                        $data[$key]['country'] = $upData['country']; 
+                    } 
+                } 
+            } 
+            $update = file_put_contents($this->jsonFile, json_encode($data)); 
+             
+            return $update?true:false; 
+        }else{ 
+            return false; 
+        } 
     }
 
     /**
@@ -45,7 +89,14 @@ class App
      * @return string
      */
     public function delete($data){
-        return $data.' deleted successfully!';
+        $jsonData = file_get_contents($this->jsonFile); 
+        $data = json_decode($jsonData, true); 
+             
+        $newData = array_filter($data, function ($var) use ($id) { 
+            return ($var['id'] != $id); 
+        }); 
+        $delete = file_put_contents($this->jsonFile, json_encode($newData)); 
+        return $delete?true:false; 
     }
 
     /**
@@ -54,7 +105,28 @@ class App
      * @return string
      */
     public function show(){
-        return 'Showing all records!';
+        if(file_exists($this->jsonFile)){ 
+            $jsonData = file_get_contents($this->jsonFile); 
+            $data = json_decode($jsonData, true); 
+             
+            if(!empty($data)){ 
+                usort($data, function($a, $b) { 
+                    return $b['id'] - $a['id']; 
+                }); 
+            } 
+             
+            return !empty($data)?$data:false; 
+        } 
+        return false; 
     }
 
+    public function show_single($id){
+        $jsonData = file_get_contents($this->jsonFile); 
+        $data = json_decode($jsonData, true); 
+        $singleData = array_filter($data, function ($var) use ($id) { 
+            return (!empty($var['id']) && $var['id'] == $id); 
+        }); 
+        $singleData = array_values($singleData)[0]; 
+        return !empty($singleData)?$singleData:false; 
+    }
 }
